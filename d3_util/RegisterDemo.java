4d4db51ca3295356ea9135b336b182aefc294f67
lit012 <145792560+lit012@users.noreply.github.com>
2024-07-11 21:35:12 +0800

    Add files via upload

---
+++ d3_util/RegisterDemo.java
@@ -0,0 +1,8 @@
+package com.itheima.d3_util;
+
+public class RegisterDemo {
+    public static void main(String[] args) {
+        //用户登录生成随机验证码
+        System.out.println(MyUtil.createCode(4));
+    }
+}
